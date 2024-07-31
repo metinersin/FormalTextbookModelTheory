b09c4ed807351aa36655d4cb3ea567a09d517d59
Metin Ersin Arıcan <metin.ersin.arican@gmail.com>
2024-07-31 15:00:05 +0300

    main theorem statement

--- FormalTextbookModelTheory.lean
+++ FormalTextbookModelTheory.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `FormalTextbookModelTheory` library.
 -- Import modules here that should be built as part of the library.
+import FormalTextbookModelTheory.ForMathlib.DLO
 import FormalTextbookModelTheory.Basic
--- FormalTextbookModelTheory/ForMathlib/DLO.lean
+++ FormalTextbookModelTheory/ForMathlib/DLO.lean
@@ -2,9 +2,20 @@ import Mathlib.SetTheory.Cardinal.Basic
 import Mathlib.ModelTheory.Basic
 import Mathlib.ModelTheory.Syntax
 import Mathlib.ModelTheory.Semantics
+import Mathlib.ModelTheory.Satisfiability
+import Mathlib.ModelTheory.Order
 
 open Cardinal
 open FirstOrder
+
+universe u v w
+
+notation "L≤" => Language.order
+
+theorem aleph0_categorical_of_dlo : aleph0.Categorical L≤.dlo := by
+  sorry
+
+#check aleph0_categorical_of_dlo
 
 -- Here, we will put definitions and theorems about the theory
 -- of dense linear orders without endpoints.

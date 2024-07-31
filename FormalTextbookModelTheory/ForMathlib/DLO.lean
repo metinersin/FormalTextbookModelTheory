1804714c0d429c7fdf5b5f0e465541203d8e55c5
Metin Ersin Arıcan <metin.ersin.arican@gmail.com>
2024-07-31 12:48:14 +0300

    import necessary libraries

---
+++ FormalTextbookModelTheory/ForMathlib/DLO.lean
@@ -0,0 +1,10 @@
+import Mathlib.SetTheory.Cardinal.Basic
+import Mathlib.ModelTheory.Basic
+import Mathlib.ModelTheory.Syntax
+import Mathlib.ModelTheory.Semantics
+
+open Cardinal
+open FirstOrder
+
+-- Here, we will put definitions and theorems about the theory
+-- of dense linear orders without endpoints.

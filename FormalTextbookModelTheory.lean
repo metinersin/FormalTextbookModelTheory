9966b845738d4a8a0641573c1a244eeb7caadb3c
Metin Ersin Arıcan <metin.ersin.arican@gmail.com>
2024-09-07 15:04:35 +0300

    cleanup

--- FormalTextbookModelTheory.lean
+++ FormalTextbookModelTheory.lean
@@ -1,7 +1,7 @@
 import FormalTextbookModelTheory.ForMathlib.Data.Fin.VecNotation
 import FormalTextbookModelTheory.ForMathlib.ModelTheory.Order
 import FormalTextbookModelTheory.ForMathlib.ModelTheory.DLO
-import AutoBlueprint
 
+-- import AutoBlueprint
 -- #eval AutoBlueprint.runForDebugging "output.txt"
 -- #eval AutoBlueprint.createBlueprint "blueprint/src/content.tex"
